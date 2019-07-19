97c6adc0849dc0230a525c92168827f91c51c493
sara-sall <sarasall092@gmail.com>
2019-07-19 17:09:57 +0200

    Added logo. Added decide button added addfoodbutton added textfields.

--- app/src/main/java/com/decirama/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/decirama/dinnerdecider/MainActivity.kt
@@ -2,11 +2,29 @@ package com.decirama.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Grilliant", "Snabbmat", "Eddy's", "Elegant Garden", "Chilliz", "Nånting nytt", "Korfu kolgrill", "Nha trang2")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideButton.setOnClickListener{
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodFieldID.text.toString()
+            foodList.add(newFood)
+            addFoodFieldID.text.clear()
+        }
+
     }
 }
